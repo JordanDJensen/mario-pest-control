0f6637200c7cd9fa4ae745402a651f46ab50a3d7
V School <vschool@Vs-Mac-Pro.local>
2017-06-14 13:10:42 -0600

    modify background

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
+ var grandTotal = 0;
+ var goombas = 0;
+ var bobbombs = 0;
+ var cheep = 0;
 
  function grandtotal() {
-     document.getElementById("grandTotal").addEventListener("change", function () {
 
-         document.getElementById("grandTotal").innerHTML = goombas + bobbombs + cheep;
-     })
+     document.getElementById("grandTotal").innerHTML = goombas + bobbombs + cheep + " " +"coins";
  };
 
  document.querySelector(".goombas").addEventListener("click", function () {
-     var goombas = Number(document.getElementById("num1").value);
-     var times = goombas * 12;
-     document.getElementById("num2").innerHTML = times + " " + "coins"
-   
+     goombas = Number(document.getElementById("num1").value) * 5;
+     document.getElementById("num2").innerHTML = goombas + " " + "coins";
+     grandtotal();
  });
 
  document.querySelector(".bobbombs").addEventListener("click", function () {
-     var bobbombs = Number(document.getElementById("num3").value);
-     var times = bobbombs * 7;
-     document.getElementById("num4").innerHTML = times + " " + "coins";
-  
+     bobbombs = Number(document.getElementById("num3").value) * 7;
+     document.getElementById("num4").innerHTML = bobbombs + " " + "coins";
+     grandtotal();
  });
 
  document.querySelector(".cheep").addEventListener("click", function () {
-     var cheep = Number(document.getElementById("num5").value);
-     var times = cheep * 11;
-     document.getElementById("num6").innerHTML = times + " " + "coins";
-     
+     cheep = Number(document.getElementById("num5").value) * 11;
+     document.getElementById("num6").innerHTML = cheep + " " + "coins";
+     grandtotal();
  });
